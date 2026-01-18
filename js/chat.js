4c6939ff6c60b91460de954a86ebd65afe6109b1
Mohammad156-r <muhammetalshave2011@gmail.com>
2026-01-18 03:16:17 +0300

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,11 @@
+ <script type="text/javascript">
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function () {
+                var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/696c1460e9768b197f02fbee/1jf72v6gs';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+            })();
+        </script>
